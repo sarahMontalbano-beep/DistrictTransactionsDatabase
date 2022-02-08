efac941f30b67ad7941b197661aa0583157ff913
byron <byron@calisium.com>
2022-02-08 11:09:31 -0700

    Switched to neo4j driver instead of http for data requests. Some work on table.

--- src/app/neo4j.service.ts
+++ src/app/neo4j.service.ts
@@ -1,38 +1,80 @@
 import { Injectable } from '@angular/core';
 import { Transaction } from 'src/shared/transaction';
-import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { auth, driver, Driver, ServerInfo, Session } from 'neo4j-driver';
 
 @Injectable({
   providedIn: 'root'
 })
 export class Neo4jService {
-private neo4jUrl = 'http://localhost:7474/db/data/transaction/commit';
+// private neo4jUrl = 'http://localhost:7474/db/data/transaction/commit';
+  private neo4jUrl: string = 'neo4j+s://e5f3f164.databases.neo4j.io:7687';
+  static driver: Driver;
 
-  transactions: Transaction[] = [];
 
-  constructor(private http: HttpClient) { }
+  constructor() {}
 
-  // getTransactions(): Observable<Transaction[]> {
-  //   // const transactions = of();
-  //   // return transactions
-  //   return this.http.get<Transaction[]>(this.neo4jUrl)
+  // constructor(private http: HttpClient) { }
+
+  // getTransactions(): Observable<any> {
+  //   const headers = {'content-type': 'application/json', 'Authorization': 'Basic Ynlyb246cGFzc3dvcmQ='}
+  //   const body = {
+  //     "statements":[
+  //       { 
+  //         "statement": "MATCH (n:FullTransaction) WHERE n.Fund = $fund RETURN n",
+  //        "parameters": {
+  //           "fund":"209"
+  //        }
+  //       }
+  //     ]
+  //   }
+  //   var r = this.http.post<any>(this.neo4jUrl, body, {'headers':headers});
+  //   return r;
   // }
 
-  getTransactions(): Observable<any> {
-    const headers = {'content-type': 'application/json', 'Authorization': 'Basic Ynlyb246cGFzc3dvcmQ='}
-    const body = {
-      "statements":[
-        { 
-          "statement": "MATCH (n:FullTransaction) WHERE n.Fund = $fund RETURN n",
-         "parameters": {
-            "fund":"209"
-         }
+  private static createDriver(): Driver {
+    return driver(
+      'neo4j+s://e5f3f164.databases.neo4j.io:7687',
+        auth.basic('byron', 'newpassword'),
+        {
+            disableLosslessIntegers: true,
         }
-      ]
+    );
+  }
+
+  static createSession(): Session {
+      if (!this.driver) {
+          this.driver = this.createDriver();
+      }
+
+      return this.driver.session({
+          database: 'neo4j'
+      });
+  }
+
+  async getTransactions(): Promise<Object[]> {
+    let d = Neo4jService.createDriver();
+    let session = Neo4jService.createSession();
+    
+    let transactions: Object[] = [];
+
+    try {
+      const result = await session.readTransaction(tx =>
+        tx.run('MATCH (t:FullTransaction) RETURN t LIMIT 25')
+      )
+
+      const records = result.records
+      for (let i = 0; i < records.length; i++) {
+        const title = records[i].get(0).properties;
+        transactions.push(title);
+      }
+
+    } finally {
+      session.close();
+      d.close();
     }
-    var r = this.http.post<any>(this.neo4jUrl, body, {'headers':headers});
-    return r;
+
+    return transactions;
   }
 
 }
--- src/app/transactions/transactions.component.ts
+++ src/app/transactions/transactions.component.ts
@@ -9,14 +9,9 @@ import { Neo4jService } from '../neo4j.service';
 })
 export class TransactionsComponent implements OnInit {
 
-  // transaction: Transaction = {
-  //   id: 1,
-  //   District_Name: 'Juno',
-  //   CreditAmount: 123
-  // };
-
-  transactions: Transaction[] = [];
-  // transactions: string = '';
+  // transactions: Transaction[] = [];
+  transactions: any[] = [];
+  labels: string[]|undefined = [];
 
   constructor(private neo4jService: Neo4jService) { }
 
@@ -24,37 +19,48 @@ export class TransactionsComponent implements OnInit {
     this.getTransactions();
   }
 
-  getTransactions(): void {
-    this.neo4jService.getTransactions()
-      .subscribe(transactions => this.toTransactionList(transactions));
+  async getTransactions(): Promise<void> {
+    // this.neo4jService.getTransactions()
+    //   .subscribe(transactions => this.toTransactionList(transactions));
+    let res = this.neo4jService.getTransactions();
+    res.then(x => this.formatForTable(x));
   }
 
-  toTransactionList(res:any): void {
-    const results = res.results[0].data;
-    // console.log(results);
-    for (let index = 0; index < results.length; index++) {
-      const element = results[index].row[0];
-      // console.log(element);
-      const t: Transaction= {
-        District_Name: element.District_Name,
-        FiscalYear: element.FiscalYear,
-        JournalDate: element.JournalDate,
-        Fund: element.Fund,
-        Reference: element.Reference,
-        FunctionCode: element.FunctionCode,
-        BatchNo: element.BatchNo,
-        CreditAmount: element.CreditAmount,
-        ThrowawayCode: element.ThrowawayCode,
-        ProgramCode: element.ProgramCode,
-        SchoolCode: element.SchoolCode,
-        Transaction: element.Transaction,
-        DebitAmount: element.DebitAmount,
-        Object_Code: element.Object_Code,
-        Object_Suffix: element.Object_Suffix
-      }
-      this.transactions.push(t);
-      console.log(t.District_Name)
+  formatForTable(data:Object[]): void {
+    if (data.length > 0) {
+      data.forEach( o => this.transactions.push(Object.values(o)))
+      this.labels = Object.getOwnPropertyNames(data[0]);
     }
-  }
+    console.log(this.transactions);
+    console.log(this.labels);
+  } 
+
+  // toTransactionList(res:any): void {
+  //   const results = res.results[0].data;
+  //   // console.log(results);
+  //   for (let index = 0; index < results.length; index++) {
+  //     const element = results[index].row[0];
+  //     // console.log(element);
+  //     const t: Transaction= {
+  //       District_Name: element.District_Name,
+  //       FiscalYear: element.FiscalYear,
+  //       JournalDate: element.JournalDate,
+  //       Fund: element.Fund,
+  //       Reference: element.Reference,
+  //       FunctionCode: element.FunctionCode,
+  //       BatchNo: element.BatchNo,
+  //       CreditAmount: element.CreditAmount,
+  //       ThrowawayCode: element.ThrowawayCode,
+  //       ProgramCode: element.ProgramCode,
+  //       SchoolCode: element.SchoolCode,
+  //       Transaction: element.Transaction,
+  //       DebitAmount: element.DebitAmount,
+  //       Object_Code: element.Object_Code,
+  //       Object_Suffix: element.Object_Suffix
+  //     }
+  //     this.transactions.push(t);
+  //     console.log(t.District_Name)
+  //   }
+  // }
 
 }
